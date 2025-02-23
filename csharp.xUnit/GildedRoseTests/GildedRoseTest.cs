ec10e413801673783e81c87556951452ea26c910
Søren Trudsø Mahon <strudso@gmail.com>
2025-02-23 11:03:41 +0100

    feat: added two tests for BackStagePasses in GildedRoseTest
    - Added a test to check the quality increases by 3 when there are 5 days or less left before sellIn.
    - Added a separate test to ensure the quality drops to 0 after the concert.

--- csharp.xUnit/GildedRoseTests/GildedRoseTest.cs
+++ csharp.xUnit/GildedRoseTests/GildedRoseTest.cs
@@ -89,4 +89,26 @@ public class GildedRoseTest
         Assert.Equal(quality + 2, item.Quality);
     }
 
+    [Fact]
+    public void BackStagePasses_5DaysOrLess_QualityBy3()
+    {
+        var quality = 0;
+        var item = Any.BackStagePass(sellIn:10, quality:quality);
+
+        new GildedRose([item]).UpdateQuality();
+
+        Assert.Equal(quality + 2, item.Quality);
+    }
+
+    [Fact]
+    public void BackStagePasses_5DaysOrLess_QualityTo0()
+    {
+        var quality = 0;
+        var item = Any.BackStagePass(sellIn:0, quality:quality);
+
+        new GildedRose([item]).UpdateQuality();
+
+        Assert.Equal(0, item.Quality);
+    }
+
 }
